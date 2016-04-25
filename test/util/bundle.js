2f22eb628be09ae41cba0e3997d0ada65785c58f
Daniel Moore <polaris@northhorizon.net>
2016-04-25 19:11:39 -0400

    Remove bundle wrapping in favor of pipeline manipulation

--- test/util/bundle.js
+++ test/util/bundle.js
@@ -3,11 +3,13 @@ var browserify = require('browserify')
 var uglify = require('gulp-uglify')
 var gulp = require('gulp')
 var rename = require('gulp-rename')
+var buffer = require('gulp-buffer')
 
 module.exports = function (entries, bopts, dest, fopts, renameFn) {
   return browserify(entries, bopts)
     .plugin(factor, fopts)
     .bundle()
+    .pipe(buffer())
     .pipe(uglify())
     .pipe(rename(renameFn || function (p) {
       return p
